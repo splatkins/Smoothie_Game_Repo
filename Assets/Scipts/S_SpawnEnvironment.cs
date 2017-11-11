79885bfceff6ec37bf86d6d256fd6872f621ca3d
Unknown <splatkins@hotmail.co.uk>
2017-11-11 09:43:06 +0000

    Environment Placement
    Test environtment created and spawned in at scene launch.

---
+++ Assets/Scipts/S_SpawnEnvironment.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class S_SpawnEnvironment : MonoBehaviour
+{
+	public GameObject enviroment;
+
+	// Use this for initialization
+	void Start ()
+	{
+		Instantiate (enviroment, new Vector3 (0, 0, 0), Quaternion.identity);
+	}
+	
+	// Update is called once per frame
+//	void Update ()
+//	{
+//		
+//	}
+}
